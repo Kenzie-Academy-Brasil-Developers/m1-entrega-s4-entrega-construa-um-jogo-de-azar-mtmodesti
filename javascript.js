e6d625324749ac3a5aa687ba08d4d97691ae8461
lucascorreaog <lucascorreaog@gmail.com>
2021-11-22 17:06:51 -0300

    A função q seleciona 3 palavras nao funciona

---
+++ javascript.js
@@ -0,0 +1,10 @@
+let crossWords = ["LASER","NEON","SABRE","PORTAL","ROBÔ","TECH","FUTURO","TECNOLOGIA","FICÇÃO","ESPAÇO","ALIEN","FOGUETE","PROPULSOR","ASTRO","MATRIX","MÁQUINA","TEMPO","AMANHÃ","SCIFI","DESTINO"]
+
+let randomWords = []
+function teste(){
+for (let i = 0; i < 3; i++){
+  let x = Math.random() * (19 - 1) + 1
+  randomWords[i] = crossWords[Math.round(x)]
+}
+return randomWords
+}
