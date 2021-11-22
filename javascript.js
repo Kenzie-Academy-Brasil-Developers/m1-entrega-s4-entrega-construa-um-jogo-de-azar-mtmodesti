7a70489a9bc58b57c820f8e32189e822c7d2cbcc
lucascorreaog <lucascorreaog@gmail.com>
2021-11-22 18:58:35 -0300

    AGORA SIM - VERSAO FINAL

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 let crossWords = ["LASER","NEON","SABRE","PORTAL","ROBÔ","TECH","FUTURO","TECNOLOGIA","FICÇÃO","ESPAÇO","ALIEN","FOGUETE","PROPULSOR","ASTRO","MATRIX","MÁQUINA","TEMPO","AMANHÃ","SCIFI","DESTINO"]
 
 let randomWords = []
-function teste(){
+function stringGenerator(){
 for (let i = 0; i < 3; i++){
   let x = Math.random() * (19 - 1) + 1
   randomWords[i] = crossWords[Math.round(x)]
