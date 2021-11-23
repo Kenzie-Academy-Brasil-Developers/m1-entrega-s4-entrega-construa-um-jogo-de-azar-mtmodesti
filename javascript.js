cc98806206aa1d5d1cb9929cd75367c88e31dc4d
Omar <omarucolombari@gmail.com>
2021-11-23 13:05:49 -0300

    feito randomWords

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
-let crossWords = ["LASER","NEON","SABRE","PORTAL","ROBÔ","TECH","FUTURO","TECNOLOGIA","FICÇÃO","ESPAÇO","ALIEN","FOGUETE","PROPULSOR","ASTRO","MATRIX","MÁQUINA","TEMPO","AMANHÃ","SCIFI","DESTINO"]
+/*let crossWords = ["LASER","NEON","SABRE","PORTAL","ROBÔ","TECH","FUTURO","TECNOLOGIA","FICÇÃO","ESPAÇO","ALIEN","FOGUETE","PROPULSOR","ASTRO","MATRIX","MÁQUINA","TEMPO","AMANHÃ","SCIFI","DESTINO"]
 
 let randomWords = []
 function stringGenerator(){
@@ -7,7 +7,7 @@ for (let i = 0; i < 3; i++){
   randomWords[i] = crossWords[Math.round(x)]
 }
 return randomWords
-}
+}*/
 
 function randomLetters(n) {
   let letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R',
@@ -18,4 +18,16 @@ function randomLetters(n) {
     random.push(letters[x])
   }
   return random
+}
+
+
+const randomWords = []
+function stringGenerator(){
+  const crossWords = ["LASER","NEON","SABRE","PORTAL","ROBÔ","TECH","FUTURO","TECNOLOGIA","FICÇÃO","ESPAÇO","ALIEN","FOGUETE","PROPULSOR","ASTRO","MATRIX","MÁQUINA","TEMPO","AMANHÃ","SCIFI","DESTINO"]
+  for(let i = 0; i < 3; i++) {
+    let x = Math.random() * crossWords.length
+    let removed = crossWords.splice(Math.round(x), 1)
+    randomWords[i] = removed[0]
+  }
+return randomWords
 }
